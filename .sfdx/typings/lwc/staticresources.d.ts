7b176e3bbfa0867c308982685babf8fdd6c73bf3
sfdc_rohit <gupta8741@gmail.com>
2020-02-03 20:02:05 +0530

    LWC Added

---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,8 @@
+declare module "@salesforce/resourceUrl/AutoComplete" {
+    var AutoComplete: string;
+    export default AutoComplete;
+}
+declare module "@salesforce/resourceUrl/Images" {
+    var Images: string;
+    export default Images;
+}
